e07040eb18f616b78db972f3dad79294ee040095
INDIA <tungavenkat146@gmail.com>
2022-10-19 16:45:38 +0530

    testing

---
+++  VenkatGIT/src/test/java/package3/sample.java
@@ -0,0 +1,12 @@
+package package3;
+
+public class sample {
+
+	public static void main(String[] args) {
+		System.out.println("hai");
+		System.out.println("Hyderabad");
+		System.out.println("india");
+		
+	}
+
+}
